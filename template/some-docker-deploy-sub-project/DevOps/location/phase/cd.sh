9a187b7bea8998cbdcd74472c75882b6c1e86daf
hughjfchen <hugh.jf.chen@gmail.com>
2019-12-18 22:59:26 +0800

    fix cd.sh for docker sub project

--- template/some-docker-deploy-sub-project/DevOps/location/phase/cd.sh
+++ template/some-docker-deploy-sub-project/DevOps/location/phase/cd.sh
@@ -8,13 +8,8 @@ fi
 
 init_with_root_or_sudo "$0"
 
-${SCRIPT_ABS_PATH}/../../../../../DevOps/gcp/test/prepare.sh
-${SCRIPT_ABS_PATH}/../../../../../DevOps/gcp/test/deploy/prepare.sh
 ${SCRIPT_ABS_PATH}/prepare.sh
 ${SCRIPT_ABS_PATH}/deploy/prepare.sh
 ${SCRIPT_ABS_PATH}/deploy/deploy.sh
 ${SCRIPT_ABS_PATH}/deploy/finishing.sh
 ${SCRIPT_ABS_PATH}/finishing.sh
-${SCRIPT_ABS_PATH}/../../../../../DevOps/gcp/test/deploy/deploy.sh
-${SCRIPT_ABS_PATH}/../../../../../DevOps/gcp/test/deploy/finishing.sh
-${SCRIPT_ABS_PATH}/../../../../../DevOps/gcp/test/finishing.sh
